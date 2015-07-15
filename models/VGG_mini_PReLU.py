449c55f205ea5fd59313598af0f27feb51b18da4
Shunta Saito <shunta.saito@gmail.com>
2015-07-16 05:29:14 +0900

    fix models using PReLU

--- models/VGG_mini_PReLU.py
+++ models/VGG_mini_PReLU.py
@@ -16,23 +16,23 @@ class VGG_mini_PReLU(FunctionSet):
     def __init__(self):
         super(VGG_mini_PReLU, self).__init__(
             conv1_1=F.Convolution2D(3, 64, 3, stride=1, pad=1),
-            prelu1_1=F.PReLU(),
+            prelu1_1=F.PReLU(64),
             conv1_2=F.Convolution2D(64, 64, 3, stride=1, pad=1),
-            prelu1_2=F.PReLU(),
+            prelu1_2=F.PReLU(64),
 
             conv2_1=F.Convolution2D(64, 128, 3, stride=1, pad=1),
-            prelu2_1=F.PReLU(),
+            prelu2_1=F.PReLU(128),
             conv2_2=F.Convolution2D(128, 128, 3, stride=1, pad=1),
-            prelu2_2=F.PReLU(),
+            prelu2_2=F.PReLU(128),
 
             conv3_1=F.Convolution2D(128, 256, 3, stride=1, pad=1),
-            prelu3_1=F.PReLU(),
+            prelu3_1=F.PReLU(256),
             conv3_2=F.Convolution2D(256, 256, 3, stride=1, pad=1),
-            prelu3_2=F.PReLU(),
+            prelu3_2=F.PReLU(256),
             conv3_3=F.Convolution2D(256, 256, 3, stride=1, pad=1),
-            prelu3_3=F.PReLU(),
+            prelu3_3=F.PReLU(256),
             conv3_4=F.Convolution2D(256, 256, 3, stride=1, pad=1),
-            prelu3_4=F.PReLU(),
+            prelu3_4=F.PReLU(256),
 
             fc4=F.Linear(4096, 1024),
             prelu4=F.PReLU(),
